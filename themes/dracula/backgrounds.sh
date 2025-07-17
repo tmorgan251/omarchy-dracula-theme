d23b12f11c6f20df099614cc8c6657fed771baa0
Tristan Morgan <tmorgan251@yahoo.com>
2025-07-16 22:57:02 -0500

    updated for my wanted install

---
+++ themes/dracula/backgrounds.sh
@@ -0,0 +1,2 @@
+mkdir -p "$BACKGROUNDS_DIR/dracula"
+download_background_image "https://github.com/dracula/wallpaper/blob/f2b8cc4223bcc2dfd5f165ab80f701bbb84e3303/first-collection/arch.png" "dracula/1-dracula.png"
