d848f057cf8a687147e6215317fc21034b91579e
tmorgan251 <tmorgan251@yahoo.com>
2025-08-10 20:12:02 -0500

    added to install script

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@ echo "Creating file directory"
 mkdir -p ~/.local/share/omarchy/themes/dracula/
 
 echo "Moving files"
-mv . ~/.local/share/omarchy/themes/dracula/
-
+mv ~/.local/share/omarchy/themes/dracula/
 echo "Files have been moved."
+
+echo "Creating symlinks"
+ln -s ~/.local/share/omarchy/themes/dracula/ ~/.config/omarchy/themes/dracula/
+echo "Symlinks created successfully"
+
 echo "Press ctrl+super+shift+space and Dracula theme should now be a selection"
+echo "Finished"
