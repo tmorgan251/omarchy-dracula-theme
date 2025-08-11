79911309619994641c894384c4b449b425829652
tmorgan251 <tmorgan251@yahoo.com>
2025-08-10 20:02:05 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Creating file directory"
+mkdir -p ~/.local/share/omarchy/themes/dracula/
+
+echo "Moving files"
+mv . ~/.local/share/omarchy/themes/dracula/
+
+echo "Files have been moved."
+echo "Press ctrl+super+shift+space and Dracula theme should now be a selection"
